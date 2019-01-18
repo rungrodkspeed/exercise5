5ea824af45570189b0e462607935af0afd03a554
unknown <rungrodks@hotmail.com>
2019-01-18 23:45:36 +0700

    https://github.com/rungrodkspeed/exercise5

---
+++ exercise5.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main (){
+    int A[10];
+    int i=0;
+    while(i<10){
+    	A[i]=-i;
+    	if(A[i]%2==0){
+    		A[i]=abs(A[i]);
+		}
+	    i++;
+    }
+    
+    i=0;
+    while(i<10){
+        cout << A[i] << " ";
+        i++;
+    }
+    return 0;
+}
