950d9f562f4ee6216bdbe89b944a0788faf5b34f
unknown <rungrodks@hotmail.com>
2019-01-18 21:38:34 +0700

    https://github.com/rungrodkspeed/exercise5

---
+++ exercise1_1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i =0, x[7]={4,-1,-2,2,5};
+    while(i<6){
+        if(x[i]>0){
+            cout << x[i];
+        }
+    i++;
+    }
+    return 0;
+}
